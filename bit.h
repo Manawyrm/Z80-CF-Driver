4f2139fb61d6ed409806020db24aa3303212e399
Tobias Mädel <t.maedel@alfeld.de>
2019-10-16 22:54:48 +0200

    remove old files

--- bit.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-#define bitSet(value, bit) ((value) |= (1UL << (bit)))
-#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
